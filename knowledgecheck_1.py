de720fe71b34891db8d36ea14f55232a49eaa7c4
Jade Brown <jnbrow13@louisville.edu>
2022-09-28 12:20:07 -0400

    First knowledge check initial commit

---
+++ knowledgecheck_1.py
@@ -0,0 +1,10 @@
+print ('hello world!')
+
+universities = ['University of the West Indies', 'Universidad de Puerto Rico', "Saint George's University", 'Universidad de la Habana', 'Universidad de Oriente Santiago de Cuba']
+print(universities[3])
+
+university_locations = {'University of the West Indies' : 'Jamaica', 'Universidad de Puerto Rico' : 'Puerto Rico'}
+print(university_locations['University of the West Indies'])
+
+universities_tuple = ('University of the West Indies', 'Universidad de Puerto Rico', "Saint George's University", 'Universidad de la Habana')
+print(universities_tuple[0])
